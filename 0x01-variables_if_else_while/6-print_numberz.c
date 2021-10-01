08f09756c7ec33dafca88a333c64c1229d2c44c6
afiorg9000 <3533@holbertonschool.com>
2021-10-01 19:32:24 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
