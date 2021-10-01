fe4cc0f4ed31e5703abb7f17fd0edf81b5ce1eaf
afiorg9000 <3533@holbertonschool.com>
2021-10-01 19:44:47 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'Z' ; ch >= 'A' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
