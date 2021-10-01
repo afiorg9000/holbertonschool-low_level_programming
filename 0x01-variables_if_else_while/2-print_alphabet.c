60fe51113cd4770b53898bdf5c226ba3ef82625e
afiorg9000 <3533@holbertonschool.com>
2021-10-01 16:30:45 +0000

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++) 
+{
+	if (ch != 'q' && ch != 'e')
+	{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
