c403de65e4f0c36fa4935728700be130995c744a
afiorg9000 <3533@holbertonschool.com>
2021-10-01 20:05:50 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+	int n, i;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	for (i = 'a' ; i <= 'f' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
