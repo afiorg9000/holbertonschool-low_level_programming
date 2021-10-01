b0cd952ad47ee64ad42ad471a8e3f2a742ce6b2e
afiorg9000 <3533@holbertonschool.com>
2021-10-01 15:37:23 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this is what the main function does
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+int main(void)
+{
+char ch;
+char CH;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (CH = 'A' ; CH <= 'Z' ; CH++)
+{
+putchar(CH);
+}
+return (0);
+}
