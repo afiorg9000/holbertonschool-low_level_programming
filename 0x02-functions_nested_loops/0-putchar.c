2d1c98a9f3253b29727c741d206ea4f940cd0aa3
afiorg9000 <3533@holbertonschool.com>
2021-10-05 17:53:32 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - print _putchar, followed by a new line
- * return: 0
+ * main - print _putchar, followed by a new line.
+ *
+ * Return: 0
  */
 int main(void)
 {
