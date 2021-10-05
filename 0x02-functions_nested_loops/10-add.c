053a10947b84ca7a95769d90c26a7498ef303fec
afiorg9000 <3533@holbertonschool.com>
2021-10-05 23:20:26 +0000

    tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @m: int to be added to b
+ * @n: int to be added to a
+ * Return: sum of a and b
+ */
+
+int add(int m, int n)
+{
+	int o;
+
+	o = m + n;
+
+	return (o);
+}
