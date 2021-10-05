de4a1f9badea3699db48b771c1bd8b2354b69948
afiorg9000 <3533@holbertonschool.com>
2021-10-05 20:23:25 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - checks for absolute value.
+ *
+ * @n: the character to be checked.
+ *
+ * Return: n
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
