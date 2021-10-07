af32e32bcf6d3b8c0c29b0b79f71d1a82dcc2399
afiorg9000 <3533@holbertonschool.com>
2021-10-07 17:30:44 +0000

    commit 1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,24 @@
 
 void print_diagonal(int n)
 {
+	int line, space;
 
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			for (space = 0; space <= line; space++)
+			{
+				if (space != 0)
+					_putchar(' ');
+				if (space == line)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
+		}
+
+	}
 
 }
