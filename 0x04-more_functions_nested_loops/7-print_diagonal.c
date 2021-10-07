425b1d3ee9dc70aa00fd863aa53c2736cffa271b
afiorg9000 <3533@holbertonschool.com>
2021-10-07 17:07:23 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_diagonal-draws a diagonal line using the \ character.
+ *@n: The number of \ characters to be printed.
+ *
+ */
+
+void print_diagonal(int n)
+{
+
+
+}
