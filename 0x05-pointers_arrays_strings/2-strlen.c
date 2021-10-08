20a428d009d1946c05a979964207df9dfc00630a
afiorg9000 <3533@holbertonschool.com>
2021-10-08 17:06:57 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string.
+ *@s: string to get the length of.
+ * Return: length off string.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s++)
+		len++;
+	return (1);
+}
