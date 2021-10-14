dcd51f38bba69df72fee6ee6ede31dbdc152c942
afiorg9000 <3533@holbertonschool.com>
2021-10-14 18:01:23 +0000

    push

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverse array of integers.
+ *@a: array.
+ *@n: number of elements of an array.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
