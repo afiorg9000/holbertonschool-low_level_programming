8141b5c21d5a3446594c00a0d1179acf061e3c3f
afiorg9000 <3533@holbertonschool.com>
2021-10-22 18:31:55 +0000

    push

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,13 @@
 int main(int argc, const char *argv[])
 {
 	int sum;
+
 	if (argc == 3)
 	{
 	sum = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", sum);
 	}
-	else 
+	else
 	{
 		printf("Error\n");
 		return (1);
