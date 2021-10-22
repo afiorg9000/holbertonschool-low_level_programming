eb0b2bb0049a4bfde308bdc9baed36b95b2eea4d
afiorg9000 <3533@holbertonschool.com>
2021-10-22 15:47:52 +0000

    push

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ *main - prints all arguments it receives.
+ * @argc:number of arguments being passed into your program.
+ * @argv:  is the array of arguments.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
