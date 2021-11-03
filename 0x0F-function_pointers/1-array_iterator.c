2ceaf78d3bda1d2a8dd98d0535305af7a07ed5d7
afiorg9000 <3533@holbertonschool.com>
2021-11-03 19:31:08 +0000

    push

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 
-if (array != NULL && action != NULL)
+if (array && action)
 
 	for (i = 0; i < size; i++)
 	{
-		action(*array), array++;
+		action(array[i]);
 	}
 }
