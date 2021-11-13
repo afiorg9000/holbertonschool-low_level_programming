aa36bdffe6426a1f3c79765784d721bdabe68eda
afiorg9000 <3533@holbertonschool.com>
2021-11-13 23:38:45 +0000

    push

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - adsds node to list
+ * @head: beggining of list
+ * @str: string to be added to list
+ * Return: returns pointer to added node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *last;
+	int i = 0;
+
+	last = *head;
+	temp = malloc(sizeof(list_t));
+
+	if (temp == NULL)
+		return (NULL);
+
+
+	if (str == NULL)
+	{
+		temp->str = NULL;
+		temp->len = 0;
+	}
+	else
+	{
+		temp->str = strdup(str);
+		while (str[i])
+		{
+			i++;
+		}
+		temp->len = i;
+	}
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = temp;
+	}
+	return (*head);
+}
+
+
