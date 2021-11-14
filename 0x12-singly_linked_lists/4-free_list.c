5d399ef7fedb29e1e2f6b5d97fa7c9a24bdab5d1
afiorg9000 <3533@holbertonschool.com>
2021-11-14 00:33:40 +0000

    push

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list
+ * @head: list to free
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
