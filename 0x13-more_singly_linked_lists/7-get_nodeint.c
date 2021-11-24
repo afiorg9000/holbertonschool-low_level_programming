7ab545c0bbe16b6aa18efa64ebca78ff1f69c199
afiorg9000 <3533@holbertonschool.com>
2021-11-24 14:09:43 +0000

    push

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - a function that returns the nth node of a listint_t.
+ * @head:
+ * @index:
+ * Return:
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t
