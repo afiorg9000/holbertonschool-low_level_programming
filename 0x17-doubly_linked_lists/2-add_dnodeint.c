1f8edc3eea6a1bc4835ac10e92f40718c79fcf27
afiorg9000 <3533@holbertonschool.com>
2022-01-07 16:01:30 +0000

    push

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,28 +2,26 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @head: the head.
- * @n: node.
+ * @head: the head of the linked list.
+ * @n: node data.
  * Return: Always EXIT_SUCCESS.
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new->n = n;
-	new->next = *head;
-	*head = new;
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
 
-	return (new);
+	return (new_node);
 }
