75ef1d427777ef0397b911a3004aeaff58d4c8b8
afiorg9000 <3533@holbertonschool.com>
2022-01-27 15:41:26 +0000

    push

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - function that creates a hash table.
- * size- the size of the array.
+ * @size: the size of the array.
  * Return:  a pointer to the newly created hash table
  */
 
